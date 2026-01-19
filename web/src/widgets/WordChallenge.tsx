5baa193ca0b4bb911ec4dfeb8ddd19a4cd89df19
ChadWyatt <chad@chadtimothy.com>
2026-01-19 11:37:39 +1100

    feat: add placeholder for Word Challenge widget UI
    Create initial structure for Word Challenge React widget.
    
    Related: #7, #4

---
+++ web/src/widgets/WordChallenge.tsx
@@ -0,0 +1,20 @@
+/**
+ * Word Challenge widget component.
+ *
+ * Displays the Wordle-style game board with:
+ * - 6x5 tile grid
+ * - Color-coded feedback (correct, present, absent)
+ * - Flip animations
+ * - Win/lose states
+ * - Share functionality
+ *
+ * @module widgets/WordChallenge
+ */
+
+// TODO: Implement WordChallenge component
+// TODO: Implement GameBoard sub-component
+// TODO: Implement Tile sub-component
+// TODO: Implement Keyboard sub-component
+// TODO: Add useWidgetState integration
+// TODO: Add animations
+// TODO: Add share functionality
